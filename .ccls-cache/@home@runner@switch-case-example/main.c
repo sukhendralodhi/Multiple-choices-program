360caa11f50669f1d0ec39875e48cc1736715020
Sukhendra S Lodhi <95135906+sukhendralodhi@users.noreply.github.com>
2023-02-06 21:21:30 +0000

    case 6 is not working i'll fix tommorow

--- .ccls-cache/@home@runner@switch-case-example/main.c
+++ .ccls-cache/@home@runner@switch-case-example/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 // palindrom function
 int palindrom() {
   int number, original, reverse = 0, reminder;
@@ -82,26 +84,48 @@ int checkLeapYear() {
   }
 }
 
+char vowelOrnot() {
+  int lowerCase, upperCase;
+  char c;
+  printf("Enter alphabets: ");
+  scanf("%c", &c);
+  lowerCase = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+  upperCase = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+  if (lowerCase || upperCase) {
+    printf("%c is Vowel.", c);
+  } else {
+    printf("%c is not vowel.", c);
+  }
+}
+
 int main(void) {
   int x;
   printf("What do you want to do? \n");
   printf("\n");
   printf(" 1 for Palindrom\n 2 for Armstrong\n 3 for Reverse\n 4 for Sum of "
-         "digits\n 5 for check leap year\n");
+         "digits\n 5 for check leap year\n 6 for check vowel or not\n");
   printf("\n");
   printf("Enter your choice:");
   scanf("%d", &x);
   printf("\n");
   if (x == 1) {
     printf("You choose palindrom.\n");
-  } else if (x == 2) {
+  }
+  if (x == 2) {
     printf("You choose armstrong.\n");
-  } else if (x == 3) {
+  }
+  if (x == 3) {
     printf("You choose reverse.\n");
-  } else if (x == 4) {
+  }
+  if (x == 4) {
     printf("You choose sum of digits.\n");
-  } else if (x == 5) {
+  }
+  if (x == 5) {
     printf("You choose check leap year.\n");
+  }
+  if (x == 6) {
+    printf("You choose Program check vowel or not.\n");
   }
 
   printf("\n");
@@ -124,6 +148,10 @@ int main(void) {
 
   case 5:
     checkLeapYear();
+    break;
+
+  case 6:
+    vowelOrnot();
     break;
 
   default:
